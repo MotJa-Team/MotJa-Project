7d7bd8248123d2cafc2f51a816c162487b388554
ATTIPARK <qkrqjawls0922@gmail.com>
2023-07-04 11:25:41 +0900

    modify contract

--- Contract/nft.sol
+++ Contract/nft.sol
@@ -2,84 +2,71 @@
 pragma solidity ^0.8.18;
 
 import './node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
-import './node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import './node_modules/@openzeppelin/contracts/access/Ownable.sol';
 import './node_modules/@openzeppelin/contracts/utils/Strings.sol';
 import './token.sol';
 
-contract NFT_1 is ERC721Enumerable {
+contract NFT is Ownable, ERC721Enumerable {
     constructor(string memory _uri, string memory _boxuri, address tokenContract) ERC721("CelebdayNFT", "CN") {
         URI = _uri;
         BOX_URI = _boxuri;
-        tokenCT = ERC20(tokenContract);
-    }
+        tokenCT = Token(tokenContract);
+    }   
 
-    // Token t_contract = new Token();
-    ERC20 public tokenCT;
+    Token public tokenCT;
 
     string public URI;
     string public BOX_URI;
-
-    uint public tokenID = 1;
 
     struct present {
         uint finalB;
         uint currentB;
     }
 
-    struct nftInfo {
-        address Owner;
-        uint presentNum;
-    }
-
     // evenEmit을 쓰게된다면 chargeBalance() 금액충전함수를 쓸때, 누가 얼만큼 충전했는지 로그를 남기는 용도로 쓰일예정
+    event whoCharged(address indexed _from, address _to, uint _num, uint _price);
  
-    mapping(address=>mapping(uint=>present)) public PRESENT;                // 해당 주소의 몇번쨰 선물의 최종금액과 현재금액
-    mapping(uint=>nftInfo) public NftData;                                  // 토큰아이디의 주인
-
-    // 선물 등록 함수 1안일 경우 nft 민팅 함수와 통합해서 사용하기
-    // function setPresent(uint _num, uint _price) public {                    // 선물을 하나씩 등록할때
-    //     address presentOwner = msg.sender;
-    //     require(PRESENT[presentOwner][_num].finalB == 0);                   // 선물이 들어가있는지 확인
-
-    //     PRESENT[presentOwner][_num].finalB = _price;                        // 최종금액을 넣으면서 선물 등록
-    // }
+    // 주소 => 토큰아이디 => struct(몇번쨰선물, 최종가격, 현재가격)             // mapping을 하나만 사용
+    mapping(address=>mapping(uint=>present)) public PRESENT;                // 해당 주소의 몇번쨰 선물의 최종금액과 현재금액        // 배열로도 시도해보자
+    mapping(uint=>uint) public presentNum;                                  // 토큰아이디로 몇번쨰 선물인지 저장하는 용도
 
     // NFT 민팅 함수 + 선물 등록과 통합할경우 (1안)
-    function mintNFT1(uint _num, uint _price) public {
+    function mintNFT(uint _num, uint _price) public {
         address nftOwner = msg.sender;
         require(PRESENT[nftOwner][_num].finalB == 0);
 
-        NftData[tokenID].Owner = nftOwner;           // 몇번쨰 nft 의 주인 주소 저장하기
-        NftData[tokenID].presentNum = _num;          // 몇번째 선물의 nft인지 저장하기
+        uint tokenID = totalSupply() + 1;
+
+        presentNum[tokenID] = _num;                  // 몇번째 선물의 nft인지 저장하기
 
         PRESENT[nftOwner][_num].finalB = _price;     // 가격을 넣으면서 선물 등록하기
 
         _mint(nftOwner, tokenID);
-        tokenID++;                                   // 민팅후 토큰번호 1증가
     }
 
-    // 선물 등록 함수 (2안)
-    function setPresent2(uint _count, uint[] memory _prices) public {       // 위시리스트를 설정한후 전체 선물을 등록할때
-        address presentOwner = msg.sender;
+    // // 선물 등록 함수 (2안)
+    // function setPresent(uint _count, uint[] calldata _prices) public {       // 위시리스트를 설정한후 전체 선물을 등록할때
+    //     address presentOwner = msg.sender;
 
-        for(uint i = 0; i < _count; i++) {
-            PRESENT[presentOwner][i+1].finalB = _prices[i];
-        }
-    }
+    //     for(uint i = 0; i < _count; i++) {
+    //         PRESENT[presentOwner][i+1].finalB = _prices[i];
+    //     }
+    // }
 
-    // NFT 민팅 함수 (2안)
-    function mintNFT2(uint _num) public {
-        address nftOwner = msg.sender;
-        NftData[tokenID].Owner = nftOwner;           // 몇번쨰 nft 의 주인 주소 저장하기
-        NftData[tokenID].presentNum = _num;          // 몇번째 선물의 nft인지 저장하기
+    // // NFT 민팅 함수 (2안)
+    // function mintNFT2(uint _num) public {
+    //     address nftOwner = msg.sender;
 
-        _mint(nftOwner, tokenID);
-        tokenID++;                                   // 민팅후 토큰번호 1증가
-    }
+    //     uint tokenID = totalSupply() + 1;
+
+    //     presentNum[tokenID] = _num;                     // 몇번째 선물의 nft인지 저장하기
+
+    //     _mint(nftOwner, tokenID);
+    // }
 
     function tokenURI(uint _tokenId) public override view returns(string memory) {
-        address tokenOwner = NftData[_tokenId].Owner;
-        uint presentNumber = NftData[_tokenId].presentNum;
+        address tokenOwner = ownerOf(_tokenId);
+        uint presentNumber = presentNum[_tokenId];
 
         if(PRESENT[tokenOwner][presentNumber].finalB <= PRESENT[tokenOwner][presentNumber].currentB) {
 
@@ -90,26 +77,34 @@ contract NFT_1 is ERC721Enumerable {
         }
     }
 
-    // 돈 충전하는 함수
+    // 선물에 돈 충전하는 함수
     function chargeBalance(address _to, uint _num, uint _price) public {
-        require(PRESENT[_to][_num].finalB != 0);                                        // 선물이 들어가있는지 확인
-        require(PRESENT[_to][_num].currentB < PRESENT[_to][_num].finalB);               // 해당 선물의 최종 금액보다 현재 충전된 금액이 적은지 확인
-        
-        // address from = msg.sender;
+        require(PRESENT[_to][_num].finalB != 0);                                         // 선물이 들어가있는지 확인
+        require(PRESENT[_to][_num].currentB < PRESENT[_to][_num].finalB);                // 해당 선물의 최종 금액보다 현재 충전된 금액이 적은지 확인
+        require(PRESENT[_to][_num].finalB-PRESENT[_to][_num].currentB >= _price);        // 남은 금액이 충전할금액보다 크거나 같은지 확인
 
-        PRESENT[_to][_num].currentB += _price;                                          // 선물의 현재 금액에 충전한 금액만큼 더하기
+        PRESENT[_to][_num].currentB += _price;                                           // 선물의 현재 금액에 충전한 금액만큼 더하기
 
-        // t_contract.transferFrom(tx.origin, address(this), _price);                   // 충전된 금액만큼 contract에 보내주기
-        tokenCT.transferFrom(tx.origin, address(this), _price);                         // 충전된 금액만큼 contract에 보내주기
+        tokenCT.burn(_price);                                                            // 금액을 저장한후 사용된 코인은 없애버림
 
-        // if(PRESENT[_to][_num].currentB > PRESENT[_to][_num].finalB) {
-            
-        //     uint remain = PRESENT[_to][_num].currentB - PRESENT[_to][_num].finalB;      // 현재 충전된 금액이 최종금액 보다 많아 졌을경우 차액 계산
-        //     t_contract.transfer(_to, remain);                                           // remain을 contract에서 생일당사자에게 보내기
-        // }
+        emit whoCharged(msg.sender, _to, _num, _price);
     }
 
-    function getChargeRatio(address _address, uint _num) public view returns(uint) {       // 충전된 금액비율 보여주기    
+    // 코인 구매
+    function buyCoin(uint _amount) public payable {
+        // input 값으로 그날의 환율을 받아와서 msg.value 설정해야함
+        // require(msg.value == 1 ether);
+
+        tokenCT.mintToken(_amount);                     // 개수만큼 토큰을 구매하는 형식
+    }
+
+    // 충전된 금액 비율 반환해주는 함수
+    function getChargeRatio(address _address, uint _num) public view returns(uint) { 
         return (PRESENT[_address][_num].currentB * 100) / PRESENT[_address][_num].finalB;
+    }
+
+    // 컨트렉트주인이 컨트렉트에서 돈을 뺴는 witdraw 함수
+    function ownerWithdraw() public onlyOwner {
+        payable(owner()).transfer(address(this).balance);
     }
 }
--- Contract/token.sol
+++ Contract/token.sol
@@ -2,15 +2,34 @@
 pragma solidity ^0.8.18;
 
 import './node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import './node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import './node_modules/@openzeppelin/contracts/access/Ownable.sol';
+import './nft.sol';
 
-contract Token is Ownable, ERC20("CelebdayToken", "CT") {
+contract Token is ERC20("CelebdayToken", "CT"), Ownable, ERC20Burnable {
 
-    function MintToken(uint _amount) public onlyOwner {
-        _mint(msg.sender, _amount);
+    address n_contract;
+
+    function setNftContract(address _nft) public onlyOwner {
+        n_contract = _nft;                                          // nft 컨트렉트 주소 저장
+    }
+
+    function mintToken(uint _amount) public {
+        require(msg.sender == n_contract);                          // msg.sender가 컨트렉트 주소일떄 통과
+        _mint(tx.origin, _amount);                                  // tx.origin 즉 nft컨트렉트에서 함수를 누른 msg.sender가 민팅 실행
     }
 
     function decimals() public pure override returns(uint8) {
         return 0;
     }
+
+    function burn(uint256 amount) override public virtual {
+        _burn(tx.origin, amount);                                   // tx.origin 즉 nft컨트렉트에서 함수를 누른 msg.sender가 번 실행
+    }
+
+    // function approve(address spender, uint256 amount) override public virtual returns (bool) {
+    //     address owner = tx.origin;
+    //     _approve(owner, spender, amount);
+    //     return true;
+    // }
 }
